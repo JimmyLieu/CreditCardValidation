5f1d45a9d0f1bcb13d3dfed39137da9ec3d6b343
JimmyLieu <98052856+JimmyLieu@users.noreply.github.com>
2022-02-20 12:13:15 -0600

    Update main.java

--- main.java
+++ main.java
@@ -6,11 +6,25 @@ public class CCValidation {
   public static void main(String[] args) {
     //Set up the scanner to read in new variables.
     Scanner scanner = new Scanner(System.in);
+    
     //Prompt user to enter their CC info and store it
     System.out.print("Please enter your credit card number: ");
     String potentialCCN = scanner.next();
     System.outprintln(""+potentialCCN);
+    
     //This will be verifying using the Luhn method
     //Using parse Int changes the String to an Integer
     int lastDigit = Integer.parseInt(potentialCCN.substring(potentialCCN.length() - 1));
+    //Print out lastDigit to test the case
+    
+    //System.out.println(lastDigit);
+    potentialCCN = potentialCCN.substring(0, potentialCCN.length() - 1));
+    //System.out.println(potentialCCN);
+    //Creating a string for the reverse CCN from Luhn's method
+    String reversedCCN = "";
+    char character;
+    for (int i = 0; i < potentialCCN.length(); i ++) {
+      character = potentialCCN.charAt(i);
+      reversedCCN = character + reversedCCN;
+    }
     
